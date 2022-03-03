3d3f4d4fd1732626ba6731184b7228b83d98edad
Daniel Krämer <daniel.kraemer@anderscore.com>
2022-03-03 17:51:26 +0100

    Initial commit

---
+++ Aufgabe6-Consumer/src/main/java/com/anderscore/App.java
@@ -0,0 +1,13 @@
+package com.anderscore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new HelloConsumer().consume();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# kafka-22.02
+Kafka Training 04.03. + 07.03.2022
